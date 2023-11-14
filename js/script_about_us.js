af42f2c4b8b67ee98536952aa63eb05ed82c57ad
informaticaesne <informatica@esne.es>
2023-11-14 17:54:17 +0100

    js colec cap cambio imgs y find us

---
+++ js/script_about_us.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function () {
+
+
+  // cambiarimagen de perfiles
+    $("#perfilmacarena").hover(function FadeIn(){
+        $("#imginicial").attr("src", "media/img/businessmen 3.jpg");
+    }, function(){
+        $("#imginicial").attr("src", "media/img/imginicial.jpg");
+    });
+
+
+    $("#perfilsandra").hover(function(){
+        $("#imginicial").attr("src", "media/img/businessmen 2.jpg");
+    }, function(){
+        $("#imginicial").attr("src", "media/img/imginicial.jpg");
+    });
+
+    $("#perfilpablo").hover(function(){
+        $("#imginicial").attr("src", "media/img/businessmen 1.jpg");
+    }, function(){
+        $("#imginicial").attr("src", "media/img/imginicial.jpg");
+    });
+
+})
