e685522ebf74d245f8a983fb117b4d4878cbe9c2
informaticaesne <informatica@esne.es>
2023-11-15 11:42:56 +0100

    colec cap responsive

--- js/script_colec_cap.js
+++ js/script_colec_cap.js
@@ -1,12 +1,14 @@
 
-$(document).ready(function () {
+
+// PRUEBA PARA HACER IMAGENES ABOUT US
+// $(document).ready(function () {
 
 
-  // cambiarimagen de perfiles
-    $("#perfilmarta").hover(function(){
-    $("#imginicial").attr("src", "media/img/Collar 1.png");
-    }, function(){
-        $("#imginicial").attr("src", "media/img/Pulsera 4.png");
-    });
+//   // cambiarimagen de perfiles
+//     $("#perfilmarta").hover(function(){
+//     $("#imginicial").attr("src", "media/img/Collar 1.png");
+//     }, function(){
+//         $("#imginicial").attr("src", "media/img/Pulsera 4.png");
+//     });
 
-})
+// })
